67c8f280fa28f3fbbdc9aca53b8d11a052a9d6d8
22120358 <phamchithuan777@gmail.com>
2025-12-10 20:05:31 +0700

    delete file App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { AuthProvider, useAuth } from '@/context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Inbox from './pages/Inbox';
 import { Loader2 } from 'lucide-react';
 import InboxPage from './pages/inbox/InboxPage';
 
